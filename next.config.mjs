2f96d184a7000f9adbef8c744f023e3fdb17f0c8
Caleb Mabry <36182383+caleb-mabry@users.noreply.github.com>
2024-05-22 19:32:36 -0400

    Exclude folder from build as its just to demo the cronjob worker

--- next.config.mjs
+++ next.config.mjs
@@ -3,8 +3,21 @@ import { setupDevPlatform } from "@cloudflare/next-on-pages/next-dev";
 if (process.env.NODE_ENV === "development") {
   await setupDevPlatform();
 }
+const rulesToProcess = [/\.m?js/, /\.(js|cjs|mjs)$/].map(String);
+
+const dirToIgnore = /workeranalytics/;
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config) => {
+    config.module.rules = config.module.rules.map((rule) => {
+      if (rule !== "..." && rulesToProcess.indexOf(String(rule.test)) > -1) {
+        rule.exclude = [dirToIgnore];
+      }
+      return rule;
+    });
+    return config;
+  },
+};
 
 export default nextConfig;
